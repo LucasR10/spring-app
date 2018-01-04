cabb4021c7f0a51d251b44d4a9de6209003fb170
Lucas <lucas.carvalho@basis.com.br>
2018-01-04 14:53:23 -0200

    ok

--- src/main/java/br/com/springapp/controller/HelloController.java
+++ src/main/java/br/com/springapp/controller/HelloController.java
@@ -1,11 +1,14 @@
 package br.com.springapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RequestMapping("/hello")
+@Controller
 public class HelloController {
 	
-	@RequestMapping("/ola")
+	@RequestMapping(method = RequestMethod.GET, name ="/ola")
 	public String ola(){
 		return "olá";
 		
